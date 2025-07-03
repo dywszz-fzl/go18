cc864bdc5b356412ccb55e1e64a3b15ada4c185c
zjh <746668433@qq.com>
2025-07-03 13:37:03 +0800

    v1-end

--- skills/main.go
+++ skills/main.go
@@ -6,8 +6,8 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"net/http"
-	"strconv"
 	"os"
+	"strconv"
 )
 
 // TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
@@ -19,13 +19,13 @@ type BookSet struct {
 }
 
 type Book struct {
-	Id uint `json:"id" gorm:"primaryKey;column;id"`
+	Id uint `json:"id" gorm:"primaryKey;column:id"`
 
 	// ??这是什么语法
-	BookSepc
+	BookSpec
 }
 
-type BookSepc struct {
+type BookSpec struct {
 	Title  string  `json:"title" gorm:"column:title;type:varchar(200)" validate:"required"`
 	Author string  `json:"author" gorm:"column:author;type:varchar(200);index" validate:"required"`
 	Price  float64 `json:"price" gorm:"column:price;type:float" valiate:"required"`
@@ -39,7 +39,7 @@ func (b *Book) TableName() string {
 }
 
 func setUpDatabase() *gorm.DB {
-	dsn := "root:123456@tcp(127.0.0.1:3306/go18?charset=utf8m4&parseTime=True&loc=Local"
+	dsn := "root:root@tcp(127.0.0.1:3306)/go18?charset=utf8mb4&parseTime=True&loc=Local"
 	//??? 应该是Gin固定用法
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -53,16 +53,16 @@ func setUpDatabase() *gorm.DB {
 var db = setUpDatabase()
 
 // ???这里是什么声明
-var h = &BookApiHandler()
+var h = &BookApiHandler{}
 
 type BookApiHandler struct {
 }
 
 func (h *BookApiHandler) ListBook(ctx *gin.Context) {
 	//??? bookHandler
-	set = &BookSet{}
+	set := &BookSet{}
 
-	pn, ps = 1, 20
+	pn, ps := 1, 20
 	pageNumber := ctx.Query("page_number")
 	if pageNumber != "" {
 		pnInt, err := strconv.ParseInt(pageNumber, 10, 64)
@@ -75,7 +75,7 @@ func (h *BookApiHandler) ListBook(ctx *gin.Context) {
 
 	pageSize := ctx.Query("page_size")
 	if pageSize != "" {
-		psInt, err = strconv.parseInt(pageSize, 10, 64)
+		psInt, err := strconv.ParseInt(pageSize, 10, 64)
 		if err != nil {
 			ctx.JSON(400, gin.H{"code": 400, "message": err.Error()})
 			return
@@ -83,7 +83,7 @@ func (h *BookApiHandler) ListBook(ctx *gin.Context) {
 		ps = int(psInt)
 	}
 
-	query := db.Module(&Book{})
+	query := db.Model(&Book{})
 
 	kws := ctx.Query("keywords")
 	// 模糊查询
@@ -92,13 +92,13 @@ func (h *BookApiHandler) ListBook(ctx *gin.Context) {
 	}
 
 	offset := (pn - 1) * ps
-	if err := query.Count(&set, Total).offset(int(offset)).Limit(int(ps)).Find(&set, Items).Error; err != nil {
+	if err := query.Count(&set.Total).Offset(int(offset)).Limit(int(ps)).Find(&set.Items).Error; err != nil {
 		// ???怎么两个500
 		ctx.JSON(500, gin.H{"code": 500, "message": err.Error()})
 		return
 	}
 
-	cts.JSON(200, set)
+	ctx.JSON(200, set)
 }
 
 func (h *BookApiHandler) CteateBook(ctx *gin.Context) {
@@ -107,15 +107,15 @@ func (h *BookApiHandler) CteateBook(ctx *gin.Context) {
 	bookSpecInstance := &BookSpec{}
 
 	//???这个是获取request中的对象
-	if err := ctx.BindJSON(bookSepcInstance); err != nil {
+	if err := ctx.BindJSON(bookSpecInstance); err != nil {
 		ctx.JSON(400, gin.H{"code": 400, "message": err.Error()})
 		return
 	}
 
 	// ????
-	bookInstance := &Book(BookSpec: *bookSpecInstance)
+	bookInstance := &Book{BookSpec: *bookSpecInstance}
 	if err := db.Save(bookInstance).Error; err != nil {
-		ctx.JSON(500, gin.H{"code":500, "message": err.Error()})
+		ctx.JSON(500, gin.H{"code": 500, "message": err.Error()})
 		return
 	}
 	//!!!返回结果
@@ -124,13 +124,12 @@ func (h *BookApiHandler) CteateBook(ctx *gin.Context) {
 
 func (h *BookApiHandler) GetBook(ctx *gin.Context) {
 	bookInstance := &Book{}
-	if err := db.Where("id = ?", ctx.Param("bn").Take(bookInstance).Error; err != nil {
-		ctx.JSON(400, gin.H{"code":400, "message": err.Error()})
+	if err := db.Where("id = ?", ctx.Param("bn")).Take(bookInstance).Error; err != nil {
+		ctx.JSON(400, gin.H{"code": 400, "message": err.Error()})
 		return
 	}
 	ctx.JSON(200, bookInstance)
 }
-
 
 func (h *BookApiHandler) UpdateBook(ctx *gin.Context) {
 	bnStr := ctx.Param("bn")
@@ -157,8 +156,8 @@ func (h *BookApiHandler) UpdateBook(ctx *gin.Context) {
 }
 
 func (h *BookApiHandler) DeleteBook(ctx *gin.Context) {
-	if err := db.Delete(&Book{}).Error; err != nil {
-		ctx.JSON(400, gin.H{"code":400, "message": err.Error()})
+	if err := db.Where("id = ?", ctx.Param("bn")).Delete(&Book{}).Error; err != nil {
+		ctx.JSON(400, gin.H{"code": 400, "message": err.Error()})
 		return
 	}
 	ctx.JSON(http.StatusNoContent, "ok")
@@ -178,7 +177,6 @@ func main() {
 	server.PUT("/api/books/:bn", h.UpdateBook)
 
 	server.DELETE("/api/books/:bn", h.DeleteBook)
-
 
 	if err := server.Run(":8080"); err != nil {
 		fmt.Println(err)
